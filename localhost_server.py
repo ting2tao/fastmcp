c90f188f378f5641a3373105230d5bf413cb67eb
史纯涛 <slovty@foxmail.com>
2025-09-17 17:49:22 +0800

    params description

--- localhost_server.py
+++ localhost_server.py
@@ -1,13 +1,17 @@
 from fastmcp import FastMCP
+from pydantic import Field
 
 # 创建 MCP server
 server = FastMCP("local-demo")
 
 # 注册工具
 @server.tool()
-def say_hello(name: str) -> str:
-    """返回一个打招呼的消息"""
+def say_hello( name: str = Field(..., description="打招呼的对象")) -> str:
+    """
+    返回一个打招呼的消息
+    """
     return f"Hello, {name}! 欢迎使用 FastMCP 2.2.0 🚀"
+
 
 if __name__ == "__main__":
     server.run()
